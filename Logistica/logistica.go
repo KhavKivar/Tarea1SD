d993ef1e6603f7edb4e98463dc06b6c0c121b037
jtdocmac <72278606+jtdocmac@users.noreply.github.com>
2020-10-14 14:01:15 -0300

    codigos de seguimiento
    agregué el sistema para manejar codigos de seguimiento para pymes

--- Logistica/logistica.go
+++ Logistica/logistica.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	//"fmt"
+	"strconv"
 
 	pb "google.golang.org/TAREA1SD/Logistica/paquete"
 	"google.golang.org/grpc"
@@ -19,35 +20,56 @@ type server struct {
 }
 
 type orden struct {
+	 timestamp string
 	 id string
 	 producto string
 	 valor int32
 	 tienda string
 	 destino string
 	 prioritario int32
+	 estado string
+	 seguimiento string
 }
 
 var ordenes []orden
+
+var retail []orden
+var pyme []orden
+
+var num_seguimiento int
+
 
 func (s *server) EnviarPedido(ctx context.Context, in *pb.Orden) (*pb.OrdenRecibida, error) {
 	log.Printf("Pedido Recibido Con id %v desde  %v hacia  %v", in.GetId(), in.GetTienda(), in.GetDestino())
 	
 	var orden_nueva orden
 
+	orden_nueva.timestamp = ""
 	orden_nueva.id = in.GetId()
 	orden_nueva.producto = in.GetProducto()
 	orden_nueva.valor = in.GetValor()
 	orden_nueva.tienda = in.GetTienda()
 	orden_nueva.destino = in.GetDestino()
 	orden_nueva.prioritario = in.GetPrioritario()
+	orden_nueva.estado = "En bodega"
+	
+	if in.GetTienda() == "pyme" {
+		orden_nueva.seguimiento = strconv.Itoa(num_seguimiento)
+		num_seguimiento = num_seguimiento + 1
+	}else {
+		orden_nueva.seguimiento = "0"
+	}
 
 	ordenes = append(ordenes,orden_nueva)
+	log.Printf("El numero de seguimiento de la orden es: %v", orden_nueva.seguimiento)
 
 	return &pb.OrdenRecibida{Message: "Orden recibida " + in.GetId()}, nil
 }
 
 func main() {
 	
+	num_seguimiento = 11111
+
 	lis, err := net.Listen("tcp", port)
 
 	if err != nil {
